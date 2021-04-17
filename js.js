9c1f70f455eb89d5a51c703bf584decd67a1e6bc
Chanda Chewe <chewec03@gmail.com>
2021-04-17 11:42:13 -0400

    S.I Conversion Units

---
+++ js.js
@@ -0,0 +1,9 @@
+
+
+function answer(){
+
+var x=document.getElementById('meters').value;
+var y;   //kilometers
+y = parseFloat(x) / (1000);
+var y=document.getElementById('kilometers').value=(y);
+}
